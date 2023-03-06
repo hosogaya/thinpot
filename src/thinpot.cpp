1ba3d158e228bead2fc78e7063019008c0a1c749
hosogaya <hosogaya.hirotaka.z7@s.mail.nagoya-u.ac.jp>
2023-03-07 05:38:49 +0900

    This is first commit

---
+++ src/thinpot.cpp
@@ -0,0 +1,180 @@
+/**
+ * @file thinpot.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include "thinpot.h"
+Thinpot::Thinpot() : bus_(&Wire1), address_(0x20){}
+Thinpot::Thinpot(const uint8_t add, TwoWire& s, const uint8_t id, const int v)
+    : bus_(&s), address_(add), ver_(v) 
+{
+    bus_->begin();
+    bus_->endTransmission();
+    bus_->beginTransmission(address_);
+    bus_->write(Configuration); // Access to Cinfiguration Register
+    // bus_->write(kSequenceMode_ | kAlertEnable_ | kPolarityHigh_); //
+    bus_->write(kSequenceMode_);
+    bus_->endTransmission();
+    id_ = id;
+    if (ver_ == 1) normal_vec_ = &normal_vec1_;
+    else normal_vec_ = &normal_vec2_;
+}
+
+void Thinpot::restart() {
+    bus_->end();
+    delay(1);
+    bus_->begin();
+}
+
+void Thinpot::writeReg16(const uint8_t reg, const uint16_t value) {
+    bus_->beginTransmission(address_);
+    bus_->write(reg);
+    bus_->write((value & 0xFF00) >> 8);
+    bus_->write(value & 0x00FF);
+    bus_->endTransmission();
+}
+
+bool Thinpot::readReg16(const uint8_t reg, uint16_t* value) {
+    bus_->beginTransmission(address_);
+    bus_->write(reg);
+    bus_->endTransmission(false);
+    bus_->requestFrom(address_, uint8_t(2));
+    if (bus_->available()) {
+        *value = uint16_t(bus_->read()) << 8;
+        *value |= uint16_t(bus_->read());
+        return true;
+    }
+    return false;
+}
+
+bool Thinpot::readReg8(const uint8_t reg, uint8_t *value){
+    bus_->beginTransmission(address_);
+    bus_->write(reg);
+    bus_->endTransmission(false);
+    bus_->requestFrom(address_, uint8_t(1));
+    if (bus_->available()) {
+        *value = bus_->read();
+        return true;
+    }
+    else return false;
+}
+
+bool Thinpot::getConfiguration(uint8_t *value){
+    return readReg8(Configuration, value);
+}
+
+
+void Thinpot::setInteruptLow(const uint16_t value){
+    for (uint8_t i =  0; i < 4; ++i) {
+        writeReg16(kLow_[i], value & 0x4FFF);
+    }
+}
+
+void Thinpot::getInteruptLow(uint16_t value[4]){
+    for (uint8_t i = 0; i < 4; ++i) {
+        readReg16(kLow_[i], value+i);
+    }
+}
+
+void Thinpot::setInteruptHigh(const uint16_t value){
+    for (uint8_t i =  0; i < 4; ++i) {
+        writeReg16(kHigh_[i], value & 0x4FFF);
+    }
+}
+void Thinpot::setHysteresis(const uint16_t value){
+    for (uint8_t i =  0; i < 4; ++i) {
+        writeReg16(kHysteresis_[i], value & 0x4FFF);
+    }
+}
+
+bool Thinpot::checkAlert(uint8_t& alert) {
+    bus_->beginTransmission(address_);
+    bus_->write(AlertStatus);
+    bus_->endTransmission();
+
+    bus_->requestFrom(address_, uint8_t(1)); // 1 byte
+    if (bus_->available()) {
+        alert = bus_->read();
+        return true;
+    }
+    else return false;
+}
+
+bool Thinpot::readADC(uint16_t dist[4]){
+    bus_->beginTransmission(address_);
+    bus_->write(ConversionResult); // mode2
+    bus_->write(kSequenceMode_); // sequence
+    delayMicroseconds(50);
+    error_ = bus_->endTransmission(false); // 読み取りの時にはfalseにする. trueにするとsequenceModeにならない
+    if (error_) {
+        restart();
+        return false;
+    }
+    uint8_t num = 0;
+    num = bus_->requestFrom(address_, (uint8_t)8, false);
+    uint8_t data[8];
+    uint8_t i = 0;
+    while (bus_->available() && i < 8) {
+        for (;i < num; ++i) data[i] = bus_->read();
+        if (num < 8) num += bus_->requestFrom(address_, (uint8_t)8 - num, false);
+    }
+
+    if (num == 8) {
+        for (uint8_t j = 0; j < 4; ++j) {
+            dist[j] = (static_cast<uint16_t>(data[2*j]) << 8);
+            dist[j] |= data[2*j + 1];
+            dist[j] &= 0x0FFF;
+        }
+    }
+    else return false;
+    // uint8_t ch, alart;
+    // uint16_t read_val;
+    // read_val = static_cast<uint16_t>(bus_->read()) << 8;
+    // read_val |= bus_->read();
+    // dist[0] = () & 0x0FFF;
+    // ch = (read_val & 0x3000) >> 12;
+    // alart = (read_val & 0x8000) >> 15;
+    delayMicroseconds(50);
+    error_ = bus_->endTransmission(true);
+    return true;
+}
+
+void Thinpot::VolToLength(const uint16_t voltage[4], uint16_t length[4]){
+    for (uint8_t i = 0; i < 4; ++i) {
+        for (uint8_t j = 1; j < 11; ++j) {
+            if (voltage[i] < mm_vol_[j]) {
+                length[i] = 5.0f / (mm_vol_[j] - mm_vol_[j-1]) * (voltage[i] - mm_vol_[j-1]) + 5.0f * (j - 1);
+                break;
+            }
+            if (j == 10) length[i] = 4095;
+        }
+    }
+}
+
+bool Thinpot::checkCollision() {
+    uint16_t voltage[4];
+    readADC(voltage);
+    // uint16_t length[4];
+    VolToLength(voltage, length_);
+    last_time_millis_ = millis();
+    
+    bool collision = false;
+    for (uint8_t i = 0; i < 4; ++i) {
+        if (voltage[i] > 4000) {
+            get_data_[i] = false; 
+            continue;
+        }
+        else {
+            get_data_[i] = true;
+            length_[i] = length_[i] + offset_; // 
+            collision = true;
+        }
+    }
+    return collision;
+}
---
+++ src/thinpot.h
@@ -0,0 +1,131 @@
+/**
+ * @file thinpot.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-11-29
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#pragma once
+
+#include <Wire.h>
+#include <array>
+#include <cmath>
+
+class Thinpot {
+    private:
+        enum Reg {
+            ConversionResult=0, 
+            AlertStatus,
+            Configuration,
+            CycleTimer, 
+            DataLowCh1, 
+            DataHighCh1, 
+            HysteresisCh1,
+            DataLowCh2, 
+            DataHighCh2, 
+            HysteresisCh2,
+            DataLowCh3, 
+            DataHighCh3, 
+            HysteresisCh3,
+            DataLowCh4, 
+            DataHighCh4, 
+            HysteresisCh4
+        };
+        uint8_t kHigh_[4] = {
+            DataHighCh1,
+            DataHighCh2,
+            DataHighCh3,
+            DataHighCh4
+        };
+        uint8_t kLow_[4] = {
+            DataLowCh1,
+            DataLowCh2,
+            DataLowCh3,
+            DataLowCh4
+        };
+        uint8_t kHysteresis_[4] = {
+            HysteresisCh1,
+            HysteresisCh2,
+            HysteresisCh3,
+            HysteresisCh4
+        };
+        const uint8_t kSequenceMode_ =0b11110000;
+        const uint8_t kFLTR_ =        0b00001000;
+        const uint8_t kAlertEnable_ = 0b00000100;
+        const uint8_t kBusyEnable_ =  0b00000010;
+        const uint8_t kResetAlert_ =  0b00000110;
+        const uint8_t kPolarityHigh_ =0b00000001;
+        
+        const uint8_t kCH1_ = 0x00;
+        const uint8_t kCH2_ = 0x01;
+        const uint8_t kCH3_ = 0x02;
+        const uint8_t kCH4_ = 0x03;
+
+    private:
+        uint8_t id_ = 0;
+        TwoWire* const bus_;
+        const uint8_t address_;
+        uint16_t mm_vol_[11] = {0, 2424, 3031, 3242, 3411, 3509, 3606, 3680, 3725, 3767, 3803};
+        int ver_ = 1;
+
+        const std::array<std::array<float, 3>, 4> tangential_vec_ = 
+        std::array<std::array<float, 3>, 4>({{
+            { 1.0f/std::sqrt(2.0f),  1.0f/std::sqrt(2.0f), 0.0f}, 
+            { 1.0f/std::sqrt(2.0f), -1.0f/std::sqrt(2.0f), 0.0f}, 
+            {-1.0f/std::sqrt(2.0f), -1.0f/std::sqrt(2.0f), 0.0f}, 
+            {-1.0f/std::sqrt(2.0f),  1.0f/std::sqrt(2.0f), 0.0f}
+        }});
+        const std::array<std::array<float, 3>, 4> normal_vec1_ =
+        std::array<std::array<float, 3>, 4>({{
+            { 1.0f/std::sqrt(2.0f), -1.0f/std::sqrt(2.0f), 0.0f}, 
+            {-1.0f/std::sqrt(2.0f), -1.0f/std::sqrt(2.0f), 0.0f}, 
+            {-1.0f/std::sqrt(2.0f),  1.0f/std::sqrt(2.0f), 0.0f}, 
+            { 1.0f/std::sqrt(2.0f),  1.0f/std::sqrt(2.0f), 0.0f}
+        }});
+
+        const std::array<std::array<float, 3>, 4> normal_vec2_ =
+        std::array<std::array<float, 3>, 4>({{
+            { -1.0f,  0.0f, 0.0f}, 
+            {  0.0f, -1.0f, 0.0f}, 
+            {  1.0f,  0.0f, 0.0f}, 
+            {  0.0f,  1.0f, 0.0f}
+        }});
+    
+    public:
+        const std::array<std::array<float, 3>, 4>* normal_vec_;
+        float offset_ = 13.0f;
+
+    public:
+        bool collision_ = false;
+        std::array<bool, 4> get_data_{{false, false, false, false}};
+        float length_[4]; // 接触位置の脚先からの距離
+        float radius_ = 2.0;
+        uint8_t error_ = 0;
+        unsigned long last_time_millis_ = 0;
+
+    public:
+        Thinpot();
+        Thinpot(const uint8_t add, TwoWire& s, const uint8_t id = -1, const int v = 1);
+        void restart();
+        void writeReg16(const uint8_t reg, const uint16_t value);
+        bool readReg8(const uint8_t reg, uint8_t* value);
+        bool readReg16(const uint8_t reg, uint16_t* value);
+        bool getConfiguration(uint8_t *value);
+        void setInteruptLow(const uint16_t value);
+        void getInteruptLow(uint16_t value[4]);
+        void setInteruptHigh(const uint16_t value);
+        void setHysteresis(const uint16_t value);
+        bool checkAlert(uint8_t& alert);
+        bool readADC(uint16_t dist[4]);
+        void VolToLength(const uint16_t voltage[4], uint16_t length[4]);
+        bool checkCollision();
+
+        inline void setOffset(const float value) {offset_ = std::abs(value);}
+        inline void setRadius(const float value) {radius_ = std::abs(value);}
+
+        inline const float getRadius() const {return radius_;}
+};
